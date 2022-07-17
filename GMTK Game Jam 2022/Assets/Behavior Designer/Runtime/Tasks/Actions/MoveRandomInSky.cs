c94f90970b6c54481caff2f7fe7ebe85d9fe48b2
mmkhajoo <mmkhajoo>
2022-07-17 17:44:44 +0430

    Phases Added Random Move Added.

---
+++ GMTK Game Jam 2022/Assets/Behavior Designer/Runtime/Tasks/Actions/MoveRandomInSky.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace BehaviorDesigner.Runtime.Tasks
+{
+    public class MoveRandomInSky : EnemyAction
+    {
+        public SharedTransform _startTransform;
+        public SharedTransform _endTransform;
+
+
+        [SerializeField] private float _speed;
+
+        [SerializeField] private float _offset;
+        
+
+        private TaskStatus _taskStatus;
+
+        private Vector3 _targetPosition;
+
+        public override void OnStart()
+        {
+            base.OnStart();
+
+            _taskStatus = TaskStatus.Running;
+
+            var x = Random.Range(_startTransform.Value.position.x + _offset, _endTransform.Value.position.x - _offset);
+            var y = Random.Range(_startTransform.Value.position.y + _offset, _endTransform.Value.position.y - _offset);
+
+            _targetPosition = new Vector3(x, y);
+
+            var distance = Mathf.Sqrt(Mathf.Pow(_targetPosition.x - transform.position.x,2) +
+                                      Mathf.Pow(_targetPosition.y - transform.position.y,2));
+
+            var move = LeanTween.move(gameObject, _targetPosition, distance / _speed);
+            move.setOnComplete(OnComplete);
+        }
+
+        private void OnComplete()
+        {
+            _taskStatus = TaskStatus.Success;
+        }
+
+        protected override TaskStatus ExecuteAction()
+        {
+            return _taskStatus;
+        }
+    }
+}
--- GMTK Game Jam 2022/Assets/Scripts/Enemy/EnemyMovement.cs
+++ GMTK Game Jam 2022/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -1,14 +1,24 @@
 ﻿using DefaultNamespace;
+using UnityEngine;
 
 namespace Enemy
 {
     public class EnemyMovement : PlayerMovement
     {
         private float _horizontalValue;
-        
+
+        private Transform _playerTransform;
+
+        private void Start()
+        {
+            _playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        }
+
         protected override void Update()
         {
             _horizontalMove = _horizontalValue * runSpeed;
+            
+            Flip();
         }
 
         public void SetHorizontalValue(bool isRight)
@@ -34,6 +44,27 @@ namespace Enemy
         public void StopMovement()
         {
             _horizontalValue = 0;
+        }
+        
+        
+        private void Flip()
+        {
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            var direction = _playerTransform.position.x - transform.position.x;
+
+            // Multiply the player's x local scale by -1.
+
+            if (direction >= 0)
+            {
+                transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, 0,
+                    transform.localRotation.eulerAngles.z));
+                
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(new Vector3(transform.localRotation.eulerAngles.x, 180,
+                    transform.localRotation.eulerAngles.z));
+            }
         }
     }
 }
